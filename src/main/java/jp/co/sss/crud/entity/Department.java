9569a9f576a90f56cc4b45a2b5a9424ffc5f6144
miyamoto.anji <miyamoto.anji@marble-corp.co.jp>
2025-06-12 15:19:14 +0900

    comi

--- src/main/java/jp/co/sss/crud/entity/Department.java
+++ src/main/java/jp/co/sss/crud/entity/Department.java
@@ -14,4 +14,20 @@ public class Department {
 	@Column
 	private String deptName;
 
+	public Integer getDeptId() {
+		return deptId;
+	}
+
+	public void setDeptId(Integer deptId) {
+		this.deptId = deptId;
+	}
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+
 }
